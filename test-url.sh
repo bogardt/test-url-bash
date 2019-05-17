6032d7b643aaf7cc7d919542bd54df9d86d62a88
bogardt <thom.bogard@gmail.com>
2019-05-17 16:35:22 +0200

    Create test-url.sh
    create script test url which test each url of a file separated by a '\n'

---
+++ test-url.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "Program should specified csv file (boris !)"
+  exit 2
+fi
+
+count=0
+lst=$(grep '\n' $1 | cut -d, -f1)
+for i in $(echo $lst | sed "s/,/ /g")
+do
+  status_code=$(curl --write-out %{http_code} --silent --output /dev/null $i)
+
+  if [[ "$status_code" -ne 200 ]] ; then
+    echo "[$count] [$status_code] [$i] error code from website"
+  else
+    echo "[$count] [$status_code] [$i] success"
+  fi
+  count=$(( $count + 1 ))
+done
+
+exit 1
